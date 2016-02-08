3f6540b1d353551578a543cb739b8d94c84aa279
Raymond Feng <raymond@studionone.com.au>
2016-02-08 14:25:06 +1000

    add task forms on normal way, not in service yet.

--- code/src/AppBundle/Resources/public/js/index.js
+++ code/src/AppBundle/Resources/public/js/index.js
@@ -1,4 +1,12 @@
 /**
  * Created by raymond on 5/02/2016.
  */
-alert('e');
+var addNewTaskForm = function() {
+    $('#task_new_btn').hide();
+    $('#task_new_form').slideDown(500);
+}
+
+var closeNewTaskForm = function() {
+    $('#task_new_btn').show();
+    $('#task_new_form').slideUp(500);
+}
